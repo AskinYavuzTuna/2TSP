6fb9005ac400327d013288deeb3f25aeee368665
AskinYavuzTuna <yavuztuna43@gmail.com>
2025-08-06 17:47:50 +0300

    Initial commit

---
+++ Point.java
@@ -0,0 +1,12 @@
+public class Point {
+	public int number;
+    public int x;
+    public int y;
+    public Point(int x, int y,int number) {
+    	this.number = number;
+        this.x = x;
+        this.y = y;
+    }
+
+}
+
